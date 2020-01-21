d01f064f3e26872f66b0c7604c5c4b51d8577995
Biki <nightfallvs0923@gmail.com>
2020-01-21 18:37:45 +0800

    get info

--- platform/getInfo.php
+++ platform/getInfo.php
@@ -1,41 +1,48 @@
 <?php
 //get basic info
-$sqlGetInfo = "SELECT `aId`, `aFName`,`aLName`,`aEmail`,`aActive`,`aVerify`, `aLoginTime`, `aLogoutTime`
-                FROM `platformAdmins` 
-                WHERE `aId` = ?
-                AND `aEmail`=?";
+try{
+    $pdo->beginTransaction();
+    $sqlGetInfo = "SELECT `aId`, `aFName`,`aLName`,`aEmail`,`aActive`,`aVerify`, `aLoginTime`, `aLogoutTime`
+                    FROM `platformAdmins` 
+                    WHERE `aId` = ?
+                    AND `aEmail`=?";
 
-$stmtGetInfo = $pdo->prepare($sqlGetInfo);
-$arrParamGetInfo = [ $_SESSION['userId'], $_SESSION['email'] ];
-$stmtGetInfo->execute($arrParamGetInfo);
-if($stmtGetInfo->rowCount()>0){
-    $arrGetInfo = $stmtGetInfo->fetchAll(PDO::FETCH_ASSOC)[0];
+    $stmtGetInfo = $pdo->prepare($sqlGetInfo);
+    $arrParamGetInfo = [ $_SESSION['userId'], $_SESSION['email'] ];
+    $stmtGetInfo->execute($arrParamGetInfo);
+    if($stmtGetInfo->rowCount()>0){
+        $arrGetInfo = $stmtGetInfo->fetchAll(PDO::FETCH_ASSOC)[0];
 
-    //尋找其permission
-    $sqlGetPermissions = "SELECT `aPermissionId`
-                    FROM `rel_platform_permissions`
-                    WHERE `aId` = ?";
-    $stmtGetPermissions = $pdo->prepare($sqlGetPermissions);
-    $arrParamGetPermissions = [ $_SESSION['userId'] ];
-    $getPrmList = [];
+        //尋找其permission
+        $sqlGetPermissions = "SELECT `aPermissionId`
+                        FROM `rel_platform_permissions`
+                        WHERE `aId` = ?";
+        $stmtGetPermissions = $pdo->prepare($sqlGetPermissions);
+        $arrParamGetPermissions = [ $_SESSION['userId'] ];
+        $getPrmList = [];
 
 
-    $stmtGetPermissions->execute($arrParamGetPermissions);
+        $stmtGetPermissions->execute($arrParamGetPermissions);
 
-    if($stmtGetPermissions->rowCount()>0){
-        //撈出所有permission，並用兩層foreach去除多餘的上一層
-        $arrGetPermissions = $stmtGetPermissions->fetchAll(PDO::FETCH_ASSOC);
-        foreach($arrGetPermissions as $key => $value){
-            foreach($value as $k => $v){
-                $getPrmList[] = $v;
+        if($stmtGetPermissions->rowCount()>0){
+            //撈出所有permission，並用兩層foreach去除多餘的上一層
+            $arrGetPermissions = $stmtGetPermissions->fetchAll(PDO::FETCH_ASSOC);
+            foreach($arrGetPermissions as $key => $value){
+                foreach($value as $k => $v){
+                    $getPrmList[] = $v;
+                }
             }
+            //把permission輸入到info裡
+            $arrGetInfo['permissions'] = $getPrmList;
+            $pdo->commit();
         }
-        //把permission輸入到info裡
-        $arrGetInfo['permissions'] = $getPrmList;
+    }else{
+        echo "你的帳號不存在";
+        session_unset();
+        session_destroy();
+        header("Refresh: 3 ; url = ./index.php");
+        exit();
     }
+}catch(Exception $err){
+    echo "failure: ".$err->getMessage();
 }
-
-// echo "<pre>";
-// print_r($arrGetInfo);
-// print_r($_SESSION);
-// echo "</pre>";
