d4ab800f9ab141a287dbd67fddb58291818cef7e
BIKI <biki@BIKIde-MacBook-Pro.local>
2020-01-22 11:16:39 +0800

    text

--- vendors/staff.php
+++ vendors/staff.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>INSPINIA | FooTable</title>
+    <title><?php echo $arrGetInfo['vName'] ?></title>
 
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
